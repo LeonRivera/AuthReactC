ffd9950fe9485dc18d58dd09ff4421fc75415240
LeonRivera <leonriv2@gmail.com>
2022-02-27 01:26:50 -0600

    proof

---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const LoginComponent = () => {
+    return(
+        <Fragment>
+            <h2>from LoginComponent</h2>
+        </Fragment>
+    );
+}
+
+export default LoginComponent;
+
